97bdc950410ecb29fdde043c5ce070df43c1abf5
mluron-ArxFjs <ms.luron@nycacademy.org>
2023-10-29 17:46:13 -0400

    updated water consumed and sleep cycles fonctionalities

---
+++ App/public/js/BMI-BMRCalculator.js
@@ -0,0 +1,35 @@
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+    let weight = document.getElementById('weight-input').value;
+    let height = document.getElementById('height-input').value;
+    let finalbmi = (weight / (height * height) * 703);
+    document.getElementById('bmi-output').value = finalbmi;
+});
+
+var bmr;
+function calculateBMR() {
+
+    //the getelements will get the user's inputs from html
+    var gender = document.getElementById('gender').value;
+    var age = parseInt(document.getElementById('age').value);
+    var height = parseInt(document.getElementById('height').value);
+    var weight = parseInt(document.getElementById('weight').value);
+
+    //formula for bmr/calorie calculations: 
+    //For men - BMR = 66 + (6.23 x weight in pounds ) + (12.7 x height in inches) – (6.8 x age)
+    // 2nd one will be for women - Women: BMR = 655 + (4.35 x weight in pounds) + (4.7 x height in inches) – (4.7 x age)
+
+    if (gender === 'male') {
+        bmr = 66.47 + (6.24 * weight) + (12.7 * height) - (6.755 * age);
+    } else if (gender === 'female') {
+        bmr = 655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+    }
+
+    //render to the page
+    document.getElementById('bmr-output').innerHTML = '<p> Your BMR is: ' + bmr.toFixed() + ' calories/days';
+    return;
+}
+//Reload page when click on Clear Form
+function clearForm() {
+    document.location.reload()
+}
--- App/public/js/NutritionixSearchFood.js
+++ App/public/js/NutritionixSearchFood.js
@@ -6,19 +6,18 @@ var breakfastCalories = [];
 var lunchCalories = [];
 var dinnerCalories = [];
 var snacksCalories = [];
-var mealSelector = $("#mealSelector").val();
 var breakfastCalItem = 0;
 var lunchCalItem = 0;
 var dinnerCalItem = 0;
 var snacksCalItem = 0;
-
-google.charts.load("current", { packages: ["corechart"] });
-
+//var mealSelector = $("#mealSelector").val();
+//google.charts.load("current", { packages: ["corechart"] });
 // Save user info to local storage 
-function userInfo() {
-}
+//function userInfo() {
+//}
+
 // Call API to log food intake 
-$("#searchBtn").click(foodLogger);
+document.getElementById('searchBtn').click(foodLogger);
 function foodLogger() {
     var foodSearch = $("#foodSearch").val().trim();
     var queryURL = "https://api.nutritionix.com/v1_1/search" + foodSearch + "?results=0:20&fields=item_name,nf_calories&appId=ebe70e9f&appKey=fd53e313cdb8d5c240702e52977603c3";
@@ -29,7 +28,6 @@ function foodLogger() {
         queryURL
 
     })
-        //
         .then(function (response) {
             var results = response.hits;
             console.log(results)
@@ -59,133 +57,129 @@ function foodLogger() {
             $("#foodBtn5").attr("data-food-calories", response.hits[5].fields.nf_calories)
             $("#foodBtn6").attr("data-food-name", response.hits[6].fields.item_name);
             $("#foodBtn6").attr("data-food-calories", response.hits[6].fields.nf_calories)
-            // }
+        
         });
 };
-function userInfo() {
-}
-
-$(".foodOption").click(saveFood);
+//function userInfo() {
+//}
 
 // Save foods to local storage 
-function saveFood() {
-    var mealSelector = $("#mealSelector").val();
-    var foodItem = $(this).attr("data-food-name");
-    var foodCalories = $(this).attr("data-food-calories");
-    if (mealSelector == "breakfast") {
-        console.log(mealSelector);
-        breakfastArray.push(foodItem);
-        breakfastCalories.push(foodCalories);
-        console.log(foodItem);
-        console.log(foodCalories);
-        $(".breakfastTableBody").empty();
-        console.log(breakfastArray);
-        for (i = 0; i < breakfastArray.length; i++) {
-            // Create a new table row element
-            var tRow = $("<tr>");
-            // Methods run on jQuery selectors return the selector they we run on
-            // This is why we can create and save a reference to a td in the same statement we update its text
-            var food = $("<td>").text(breakfastArray[i]);
-            var calories = $("<td>").text(breakfastCalories[i]);
-            // Append the newly created table data to the table row
-            tRow.append(food, calories);
-            // Append the table row to the table body
-            $(".breakfastTableBody").append(tRow);
-            console.log(parseInt(breakfastCalories[i]))
-        }
+// document.getElementById('foodOptions').click(saveFood);
+// function saveFood() {
+//     var mealSelector = $("#mealSelector").val();
+//     var foodItem = $(this).attr("data-food-name");
+//     var foodCalories = $(this).attr("data-food-calories");
+//     if (mealSelector == "breakfast") {
+//         console.log(mealSelector);
+//         breakfastArray.push(foodItem);
+//         breakfastCalories.push(foodCalories);
+//         console.log(foodItem);
+//         console.log(foodCalories);
+//         $(".breakfastTableBody").empty();
+//         console.log(breakfastArray);
+//         for (i = 0; i < breakfastArray.length; i++) {
+//             // Create a new table row element
+//             var tRow = $("<tr>");
+//             // Methods run on jQuery selectors return the selector they we run on
+//             // This is why we can create and save a reference to a td in the same statement we update its text
+//             var food = $("<td>").text(breakfastArray[i]);
+//             var calories = $("<td>").text(breakfastCalories[i]);
+//             // Append the newly created table data to the table row
+//             tRow.append(food, calories);
+//             // Append the table row to the table body
+//             $(".breakfastTableBody").append(tRow);
+//             console.log(parseInt(breakfastCalories[i]))
+//         }
+//         breakfastCalItem += parseInt(foodCalories)
 
-        breakfastCalItem += parseInt(foodCalories)
+//     }
+//     if (mealSelector == "lunch") {
+//         console.log(mealSelector);
+//         lunchArray.push(foodItem);
+//         lunchCalories.push(foodCalories);
+//         console.log(foodItem);
+//         console.log(foodCalories);
+//         $(".lunchTableBody").empty();
+//         console.log(lunchArray);
+//         for (i = 0; i < lunchArray.length; i++) {
+//             // Create a new table row element
+//             var tRow = $("<tr>");
+//             // Methods run on jQuery selectors return the selector they we run on
+//             // This is why we can create and save a reference to a td in the same statement we update its text
+//             var food = $("<td>").text(lunchArray[i]);
+//             var calories = $("<td>").text(lunchCalories[i]);
+//             // Append the newly created table data to the table row
+//             tRow.append(food, calories);
+//             // Append the table row to the table body
+//             $(".lunchTableBody").append(tRow);
+//         }
+//         lunchCalItem += parseInt(foodCalories)
+//     }
+//     if (mealSelector == "dinner") {
+//         console.log(mealSelector);
+//         dinnerArray.push(foodItem);
+//         dinnerCalories.push(foodCalories);
+//         console.log(foodItem);
+//         console.log(foodCalories);
+//         $(".dinnerTableBody").empty();
+//         console.log(dinnerArray);
+//         for (i = 0; i < dinnerArray.length; i++) {
+//             // Create a new table row element
+//             var tRow = $("<tr>");
+//             // Methods run on jQuery selectors return the selector they we run on
+//             // This is why we can create and save a reference to a td in the same statement we update its text
+//             var food = $("<td>").text(dinnerArray[i]);
+//             var calories = $("<td>").text(dinnerCalories[i]);
+//             // Append the newly created table data to the table row
+//             tRow.append(food, calories);
+//             // Append the table row to the table body
+//             $(".dinnerTableBody").append(tRow);
+//         }
+//         dinnerCalItem += parseInt(foodCalories)
+//     }
+//     if (mealSelector == "snack") {
+//         console.log(mealSelector);
+//         snacksArray.push(foodItem);
+//         snacksCalories.push(foodCalories);
+//         console.log(foodItem);
+//         console.log(foodCalories);
+//         $(".snacksTableBody").empty();
+//         console.log(snacksArray);
+//         for (i = 0; i < snacksArray.length; i++) {
+//             // Create a new table row element
+//             var tRow = $("<tr>");
+//             // Methods run on jQuery selectors return the selector they we run on
+//             // This is why we can create and save a reference to a td in the same statement we update its text
+//             var food = $("<td>").text(snacksArray[i]);
+//             var calories = $("<td>").text(snacksCalories[i]);
+//             // Append the newly created table data to the table row
+//             tRow.append(food, calories);
+//             // Append the table row to the table body
+//             $(".snacksTableBody").append(tRow);
+//         }
+//         snacksCalItem += parseInt(foodCalories)
+//     }
 
-    }
-    if (mealSelector == "lunch") {
-        console.log(mealSelector);
-        lunchArray.push(foodItem);
-        lunchCalories.push(foodCalories);
-        console.log(foodItem);
-        console.log(foodCalories);
-        $(".lunchTableBody").empty();
-        console.log(lunchArray);
-        for (i = 0; i < lunchArray.length; i++) {
-            // Create a new table row element
-            var tRow = $("<tr>");
-            // Methods run on jQuery selectors return the selector they we run on
-            // This is why we can create and save a reference to a td in the same statement we update its text
-            var food = $("<td>").text(lunchArray[i]);
-            var calories = $("<td>").text(lunchCalories[i]);
-            // Append the newly created table data to the table row
-            tRow.append(food, calories);
-            // Append the table row to the table body
-            $(".lunchTableBody").append(tRow);
-        }
-        lunchCalItem += parseInt(foodCalories)
-    }
-    if (mealSelector == "dinner") {
-        console.log(mealSelector);
-        dinnerArray.push(foodItem);
-        dinnerCalories.push(foodCalories);
-        console.log(foodItem);
-        console.log(foodCalories);
-        $(".dinnerTableBody").empty();
-        console.log(dinnerArray);
-        for (i = 0; i < dinnerArray.length; i++) {
-            // Create a new table row element
-            var tRow = $("<tr>");
-            // Methods run on jQuery selectors return the selector they we run on
-            // This is why we can create and save a reference to a td in the same statement we update its text
-            var food = $("<td>").text(dinnerArray[i]);
-            var calories = $("<td>").text(dinnerCalories[i]);
-            // Append the newly created table data to the table row
-            tRow.append(food, calories);
-            // Append the table row to the table body
-            $(".dinnerTableBody").append(tRow);
-        }
-        dinnerCalItem += parseInt(foodCalories)
-    }
-    if (mealSelector == "snack") {
-        console.log(mealSelector);
-        snacksArray.push(foodItem);
-        snacksCalories.push(foodCalories);
-        console.log(foodItem);
-        console.log(foodCalories);
-        $(".snacksTableBody").empty();
-        console.log(snacksArray);
-        for (i = 0; i < snacksArray.length; i++) {
-            // Create a new table row element
-            var tRow = $("<tr>");
-            // Methods run on jQuery selectors return the selector they we run on
-            // This is why we can create and save a reference to a td in the same statement we update its text
-            var food = $("<td>").text(snacksArray[i]);
-            var calories = $("<td>").text(snacksCalories[i]);
-            // Append the newly created table data to the table row
-            tRow.append(food, calories);
-            // Append the table row to the table body
-            $(".snacksTableBody").append(tRow);
-        }
-        snacksCalItem += parseInt(foodCalories)
-    }
+//     google.charts.setOnLoadCallback(drawChart);
+//     function drawChart() {
 
-    google.charts.setOnLoadCallback(drawChart);
-    function drawChart() {
+//         var data = google.visualization.arrayToDataTable([
+//             ['Task', 'Calories per Meal'],
+//             ['Breakfast', breakfastCalItem],
+//             ['Lunch', lunchCalItem],
+//             ['Dinner', dinnerCalItem],
+//             ['Snacks', snacksCalItem],
+//         ]);
+//         var options = {
+//             fontName: 'Unica One',
+//             fontSize: '25',
+//             title: 'Daily Calorie Breakdown',
+//             pieHole: 0.4,
+//             is3D: true,
+//             backgroundColor: { fill: 'transparent' }
+//         };
 
-        var data = google.visualization.arrayToDataTable([
-            ['Task', 'Calories per Meal'],
-            ['Breakfast', breakfastCalItem],
-            ['Lunch', lunchCalItem],
-            ['Dinner', dinnerCalItem],
-            ['Snacks', snacksCalItem],
-        ]);
-
-        var options = {
-            fontName: 'Unica One',
-            fontSize: '25',
-            title: 'Daily Calorie Breakdown',
-            pieHole: 0.4,
-            is3D: true,
-            backgroundColor: { fill: 'transparent' }
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options)
-
-    }
-};
+//         var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+//         chart.draw(data, options)
+//     }
+// };
